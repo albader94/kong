ac3b46adfe7f4dd486451520f7483d9762aa51da
jatr6994 <jatr6994@colorado.edu>
2018-06-23 15:04:01 -0600

    Finished testing database!
    Tested login and sensor tables!

---
+++ Documents/DB Design/Test_Login_Script.sql
@@ -0,0 +1,5 @@
+USE Kong;
+INSERT INTO Login Values (default, 'Kong', 'Admin');
+INSERT INTO Login VAlues (default, 'Jake', 'password');
+INSERT INTO Login VAlues (default, 'test', 'test_pass');
+SELECT * FROM Login;
