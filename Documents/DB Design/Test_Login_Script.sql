b262734264273ee2d9bdbdcc47e5e49ac7752b39
Jacob <jatr6994@colorado.edu>
2018-06-25 12:49:30 -0600

    Added final screenshot
    Fixed a few typos and made sure SensorDT was unique too

--- Documents/DB Design/Test_Login_Script.sql
+++ Documents/DB Design/Test_Login_Script.sql
@@ -1,5 +1,5 @@
 USE Kong;
 INSERT INTO Login Values (default, 'Kong', 'Admin');
-INSERT INTO Login VAlues (default, 'Jake', 'password');
-INSERT INTO Login VAlues (default, 'test', 'test_pass');
+INSERT INTO Login Values (default, 'Jake', 'password');
+INSERT INTO Login Values (default, 'test', 'test_pass');
 SELECT * FROM Login;
