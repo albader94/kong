6d6debb528ca99bd821f344fedd9a84c3e02fb69
Jacob <jatr6994@colorado.edu>
2018-06-27 10:54:51 -0600

    fixed a few typos in generating db

--- Documents/DB Design/Kong_Grow_DB_Generate.sql
+++ Documents/DB Design/Kong_Grow_DB_Generate.sql
@@ -8,14 +8,14 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema kong
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema kong
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `kong` DEFAULT CHARACTER SET utf8 ;
+USE `kong` ;
 
 -- -----------------------------------------------------
 -- Table `kong`.`Login`
